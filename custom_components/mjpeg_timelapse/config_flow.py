a5c36847f9758fa0665f79b4c357631434833371
markaggar <markaggar@hotmail.com>
2025-01-10 13:41:00 -0800

    Update config_flow.py
    Revert back to working version

--- custom_components/mjpeg_timelapse/config_flow.py
+++ custom_components/mjpeg_timelapse/config_flow.py
@@ -8,8 +8,7 @@ from homeassistant.const import (
     CONF_PASSWORD,
 )
 from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers.selector import selector
-from homeassistant.helpers.translation import async_get_translations
+from homeassistant.helpers.selector import selector  # Import the selector component
 
 from .const import (
     DOMAIN,
@@ -24,37 +23,35 @@ from .const import (
     CONF_START_TIME,
     CONF_END_TIME,
     CONF_ENABLING_ENTITY_ID,
-    DEFAULT_ENABLING_ENTITY_ID,  # Use the new constant
+    DEFAULT_ENABLING_ENTITY_ID,
+)
+
+# Use vol.Coerce(str) to ensure start_time and end_time are strings
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_IMAGE_URL): str,
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): str,
+        vol.Optional(CONF_FETCH_INTERVAL, default=60): int,
+        vol.Optional(CONF_START_TIME, default="00:00:00"): vol.Coerce(str),
+        vol.Optional(CONF_END_TIME, default="23:59:59"): vol.Coerce(str),
+        vol.Optional(CONF_ENABLING_ENTITY_ID, default=DEFAULT_ENABLING_ENTITY_ID): selector({
+            "entity": {
+                "domain": ["sensor", "binary_sensor"],  # Specify multiple domains
+                "multiple": False  # Set to True if you want to allow multiple selections
+            }
+        }),  # Use the entity selector
+        vol.Optional(CONF_FRAMERATE, default=2): int,
+        vol.Optional(CONF_MAX_FRAMES, default=100): int,
+        vol.Optional(CONF_QUALITY, default=75): vol.All(vol.Coerce(int), vol.Range(min=1, max=100)),
+        vol.Optional(CONF_LOOP, default=True): bool,
+        vol.Optional(CONF_USERNAME): str,
+        vol.Optional(CONF_PASSWORD): str,
+    }
 )
 
 def valid_url(url):
     result = urlparse(url)
     return result.scheme != '' and result.netloc != ''
-
-async def get_data_schema(hass):
-    translations = await async_get_translations(hass, "en", "strings.json", DOMAIN)
-    return vol.Schema(
-        {
-            vol.Required(CONF_IMAGE_URL): str,
-            vol.Optional(CONF_NAME, default=DEFAULT_NAME): str,
-            vol.Optional(CONF_FETCH_INTERVAL, default=60): int,
-            vol.Optional(CONF_START_TIME, default="00:00:00"): vol.Coerce(str),
-            vol.Optional(CONF_END_TIME, default="23:59:59"): vol.Coerce(str),
-            vol.Optional(CONF_ENABLING_ENTITY_ID, default=DEFAULT_ENABLING_ENTITY_ID): selector({
-                "entity": {
-                    "domain": ["sensor", "binary_sensor"],
-                    "multiple": False
-                }
-            }),
-            vol.Optional(CONF_FRAMERATE, default=2): int,
-            vol.Optional(CONF_MAX_FRAMES, default=100): int,
-            vol.Optional(CONF_QUALITY, default=75): vol.All(vol.Coerce(int), vol.Range(min=1, max=100)),
-            vol.Optional(CONF_LOOP, default=True): bool,
-            vol.Optional(CONF_HEADERS, default={}): dict,
-            vol.Optional(CONF_USERNAME): str,
-            vol.Optional(CONF_PASSWORD): str,
-        }
-    )
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle the config flow for Mjpeg Timelapse."""
@@ -70,10 +67,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     title=user_input[CONF_NAME],
                     data=user_input,
                 )
-        data_schema = await get_data_schema(self.hass)
         return self.async_show_form(
             step_id="user",
-            data_schema=data_schema,
+            data_schema=DATA_SCHEMA,
             errors=errors,
         )
 
